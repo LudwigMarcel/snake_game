9b95fd7cd0cb436263d90c3ab9c2e1e1b77f0290
Marcel Ludwig <london_marcel@hotmail.com>
2024-07-17 21:44:49 -0300

    Snake Game update

--- main.py
+++ main.py
@@ -1,87 +1,105 @@
 import pygame
 import random
 
-def snake_move():
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        if player_pos.y > 0:
-            player_pos.y -= 10
-    if keys[pygame.K_s]:
-        if player_pos.y < 480:
-            player_pos.y += 10 
-    if keys[pygame.K_a]:
-        if player_pos.x > 0:
-            player_pos.x -= 10
-    if keys[pygame.K_d]:
-        if player_pos.x < 720:
-            player_pos.x += 10
+from pygame.sprite import Group
 
-def snake_create(): #useful?
-    snake_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-    snake_body = ... #TODO
-    return snake_pos, snake_body
-
-def snake_grow():
-    ...
-
-def check_colision(snake_pos, width, height):
-    if snake_pos[0] <= 0 or snake_pos[1] <= 0 or snake_pos[0] > width-10 or snake_pos[1] > height-10 :
-        print("bateu")
+def check_colision(head_pos, snake_segment, width, height):
+    if head_pos <= 0 or head_pos > width-10 or head_pos > height-10 :
         return True 
-    
-def food_colision(snake_pos, food_pos):
-    if snake_pos == food_pos: # dont work vector must be exactly one above the other CHANGE
-        print("food")
-        return True
+    for block in snake_segment[1:]:
+        if head_pos == block:
+            return True
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((720, 480))
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((512, 512))
+pygame.display.set_caption('Snake Game')
 running = True
-dt = 0
 
-food_exist = True
+#Images
+background = pygame.image.load('screen_back.jpg').convert()
+apple_image = pygame.image.load('apple.png').convert_alpha()
+# create
+# snake
+class SnakeSegment(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()     
+        self.image =  pygame.Surface([15, 15])
+        self.image.fill('green')
+        self.rect = self.image.get_rect(topleft=(x,y))
 
-# create an apple
-food_pos = pygame.Vector2(random.randrange(1, screen.get_width()), random.randrange(1, screen.get_height()))
+# food
+class Food(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface([15,15])
+        self.image.fill('red')
+        self.rect = self.image.get_rect(topleft=(x,y))
+    def relocate(self):
+        self.rect.topleft = (random.randrange(10, screen.get_width()-10), random.randrange(10, screen.get_height()-10))
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+snake_segments = pygame.sprite.Group() #grupo de sprites
+# criar uma pos inicial, iterar sobre ela, e adicionar um segmento
+initial_pos = [pygame.Vector2(100, 50), pygame.Vector2(90, 50), pygame.Vector2(80, 50)]
+for pos in initial_pos:
+    segment = SnakeSegment(pos.x, pos.y)
+    snake_segments.add(segment)
+head = snake_segments.sprites()[0] #a cobra sempre vai ser o index 0 no grupo de sprites
 
-while running:
+# pos init food
+food = Food(random.randrange(10, screen.get_width()-10), random.randrange(10, screen.get_height()-10))
+food_group = pygame.sprite.GroupSingle(food)
 
-    # end_game = pygame.display.set_mode(150,75)
-    # end_game.fill("white")
-    # pygame.show(end_game)
+direction = pygame.Vector2(10, 0)
+clock = pygame.time.Clock()
 
-    
+while running:  
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-   
-    # functions
-    snake_move()
-    food_colision(player_pos, food_pos)
-    if check_colision(player_pos, 720, 480):
-        running = False
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and direction.y == 0:
+        direction = pygame.Vector2(0, -10)
+    if keys[pygame.K_s] and direction.y == 0:
+        direction = pygame.Vector2(0, 10)
+    if keys[pygame.K_a] and direction.x == 0:
+        direction = pygame.Vector2(-10, 0)
+    if keys[pygame.K_d] and direction.x == 0:
+        direction = pygame.Vector2(10, 0)
+    
+
+    #colision check    
+    # if check_colision(snake_pos, snake_body, 512, 512):
+    #     running = False
+
+    # testar e explicar o funcionamento
+    new_head_pos = pygame.Vector2(head.rect.topleft) + direction
+    new_head = SnakeSegment(new_head_pos[0], new_head_pos[1])
+    snake_segments.add(new_head)
+    snake_segments = pygame.sprite.Group(new_head, *snake_segments)
+    head = new_head
+    
+    #check colision border
+    #check_colision(head, snake_segments, screen.get_width, screen.get_height)
+
+    # check colision w/ apple
+    if pygame.sprite.spritecollideany(head, food_group):
+        food.relocate()
+    else:  # growth control
+        tail = snake_segments.sprites()[-1]
+        snake_segments.remove(tail)
+        
     
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
-
-    pygame.draw.circle(screen, "white", food_pos, 30)
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    
-
-    # flip() the display to put your work on screen
+    screen.blit(background, (0,0))
+    snake_segments.draw(screen)
+    food_group.draw(screen)
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    # game speed
+    clock.tick(10)
 
 pygame.quit()
